1c99f313eb4eefad380601c70a79d62a22bb4e25
maxinth <tunde.j.adeleye@gmail.com>
2021-04-06 07:14:05 +0100

    tried build fix

---
+++ build/precache-manifest.8cc91ac8e1a9b3df3943fe36a17cf278.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae7581a2c933c250ce70499d606a5ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90013d151fd8ddc35ca0",
+    "url": "/static/css/main.b6a1559d.chunk.css"
+  },
+  {
+    "revision": "d1f31060c7e3116dcac1",
+    "url": "/static/js/2.7a9c4ad5.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.7a9c4ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90013d151fd8ddc35ca0",
+    "url": "/static/js/main.126f89e9.chunk.js"
+  },
+  {
+    "revision": "a8dd5eddc597d3864200",
+    "url": "/static/js/runtime-main.accc557f.js"
+  },
+  {
+    "revision": "725b42493fd6046960aa0f40bc23b9ea",
+    "url": "/static/media/1040x300-e-Top-Up.725b4249.gif"
+  },
+  {
+    "revision": "b19c0441bd3921e14cee30fa7793bf61",
+    "url": "/static/media/mobilemoney.b19c0441.jpg"
+  },
+  {
+    "revision": "7cc1c2b7e41e56ce4afe4ce48a8e0b72",
+    "url": "/static/media/partner-banks.7cc1c2b7.jpg"
+  }
+]);
